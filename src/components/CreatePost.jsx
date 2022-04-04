3b238de65403894a2249351af5327e08dca34539
unknown <k.petkelis@post.penki.lt>
2022-04-04 15:43:02 +0300

    add youtube 'auto-detect'

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -7,35 +7,49 @@ const CreatePost = ({thisUser, socket}) => {
     
     const {id} = useParams()
     const [message, setMessage] = useState("")
-    console.log(thisUser.username, id)
+    //console.log(thisUser.username, id)
     
     const inputs ={
         text: useRef(),
         photo: useRef(),
-        youtubeUrl: useRef()
+        // youtubeUrl: useRef()
       }
     
     const createpost = async() =>{
 
-        const post={
-            text: inputs.text.current.value,
-            photo: inputs.photo.current.value,
-            youtubeUrl: inputs.youtubeUrl.current.value,
+        const regExp= /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
+        let url1=""
+        let text1=""
+        if(regExp.exec(inputs.text.current.value) === null){
+            url1=""
+            text1 = inputs.text.current.value
+           // console.log("not youtube", url1)
+        }else {
+            url1 = regExp.exec(inputs.text.current.value)[0]
+            text1 = inputs.text.current.value.replace(url1,"")
+           // console.log(url1)
         }
-        console.log(post)
+
+
+        const post={
+            //text: inputs.text.current.value,
+            text: text1,
+            photo: inputs.photo.current.value,
+            // youtubeUrl: inputs.youtubeUrl.current.value,
+            youtubeUrl: url1
+        }
+        // console.log(post)
         const res = await http.post(post,'createpost/'+id)
-            console.log("result from http", res, res.message)
+           // console.log("result from http", res, res.message)
 
         setMessage(res.message)
         inputs.text.current.value=''
         inputs.photo.current.value=""
-        inputs.youtubeUrl.current.value=""
+        // inputs.youtubeUrl.current.value=""
         if(res.success){
             // console.log("socket must be there")
             socket.emit("newPost", thisUser.username+" has written new post in topic "+ '"'+res.oneTopic.title+'" ')
         }
-    
-    // console.log("create auction", auction)
 
     }
 
@@ -51,9 +65,9 @@ const CreatePost = ({thisUser, socket}) => {
         <div className='d-flex just-center'>
                 <div className='reg-inputs'>
                     <label>Type you post: </label><br/>
-                    <textarea rows="10" cols="60" placeholder='Required, the post can be 10-250 characters length' ref={inputs.text}/><br/><br/>
-                    <label>Youtube clip URL:</label><br/>
-                    <input className='mt-5' type="text" size="80" ref={inputs.youtubeUrl} placeholder='Optional'/><br/><br/><br/>
+                    <textarea rows="10" cols="60" placeholder='Required, the post can be 10-250 characters length, youtube link can be included' ref={inputs.text}/><br/><br/>
+                    {/* <label>Youtube clip URL:</label><br/>
+                    <input className='mt-5' type="text" size="80" ref={inputs.youtubeUrl} placeholder='Optional'/><br/><br/><br/> */}
                     <label>Picture URL:</label><br/>
                     <input className='mt-5' type="text" size="80" ref={inputs.photo} placeholder="Optional"/><br/><br/>
                     <button onClick={()=>createpost()} className="ml-0">Create new post</button>
